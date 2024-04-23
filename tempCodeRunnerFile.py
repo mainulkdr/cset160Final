96c1d68e14f39f24b3b3570bff24e0fa5dc66574
Mainul <mainulkadr@gmail.com>
2024-04-23 15:37:32 -0400

    tem file removed

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-        # elif user_choice == "Teacher":
-        #     accounts = conn.execute(text('select * from user where type = "Teacher"')).all()
-        #     return render_template("accounts.html", accounts=accounts)
-        # elif user_choice == "Student":
-        #     accounts = conn.execute(text('select * from user where type = "Student"')).all()
-        #     return render_template("accounts.html", accounts=accounts)
